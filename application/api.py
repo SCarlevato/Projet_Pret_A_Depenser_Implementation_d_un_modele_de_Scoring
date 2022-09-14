fa1e9827cd959441a1bcd9842dc2870520cd42c7
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-14 21:34:23 +0200

    Update api.py

--- application/api.py
+++ application/api.py
@@ -38,5 +38,12 @@ def load_data_target():
     target = data.iloc[:, -1:]
     return json.dumps(list(target))
 
+@app.route("/load_age_population/", methods=["GET"])
+def load_age_population():
+    z = ZipFile("/Users/sylvaincarlevato/p7scoringopenclassrooms/data/data_final.zip")
+    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding='utf-8')
+    data_age = round((data["DAYS_BIRTH"]/365), 2)
+    return json.dumps(list(data_age))
+
 if __name__ == "__main__":
     app.run(host="localhost", port="4001", debug=True)
