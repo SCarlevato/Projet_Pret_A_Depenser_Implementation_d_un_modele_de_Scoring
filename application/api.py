9ccae38d2ebb60e39bd2f0d033bc4073dd388098
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-14 10:13:28 +0200

    Update api.py

--- application/api.py
+++ application/api.py
@@ -13,65 +13,20 @@ sns.set_style('darkgrid')
 
 app = Flask(__name__)
 
+@app.route("/load_age_population", methods=["GET"])
+
+def load_age_population():
+
+    z = ZipFile("/Users/sylvaincarlevato/p7scoringopenclassrooms/data/data_final.zip")
+
+    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding='utf-8')
+
+    data_age = round((data["DAYS_BIRTH"]/365), 2)
+
+    return json.dumps(list(data_age))
 
 
-
-
-
-
-
-
-    @st.cache(allow_output_mutation=True)
-    def load_knn(sample):
-        knn = knn_training(sample)
-        return knn
-
-    @st.cache
-    def load_infos_gen(data):
-        lst_infos = [data.shape[0],
-                     round(data["AMT_INCOME_TOTAL"].mean(), 2),
-                     round(data["AMT_CREDIT"].mean(), 2)]
-
-        nb_credits = lst_infos[0]
-        rev_moy = lst_infos[1]
-        credits_moy = lst_infos[2]
-
-        targets = data.TARGET.value_counts()
-
-        return nb_credits, rev_moy, credits_moy, targets
-
-
-
-    @st.cache
-    def load_age_population(data):
-        data_age = round((data["DAYS_BIRTH"]/365), 2)
-        return data_age
-
-    @st.cache
-    def load_income_population(sample):
-        df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
-        df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
-        return df_income
-
-    @st.cache
-    def load_prediction(sample, id, clf):
-        X=sample.iloc[:, :-1]
-        score = clf.predict_proba(X[X.index == int(id)])[:,1]
-        return score
-
-    @st.cache
-    def load_kmeans(sample, id, mdl):
-        index = sample[sample.index == int(id)].index.values
-        index = index[0]
-        data_client = pd.DataFrame(sample.loc[sample.index, :])
-        df_neighbors = pd.DataFrame(knn.fit_predict(data_client), index=data_client.index)
-        df_neighbors = pd.concat([df_neighbors, data], axis=1)
-        return df_neighbors.iloc[:,1:].sample(10)
-
-    @st.cache
-    def knn_training(sample):
-        knn = KMeans(n_clusters=2).fit(sample)
-        return knn
+ 
 
 
 
