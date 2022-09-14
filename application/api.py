286fefaa99a443b970fe59fc2d36c86c470cccac
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-14 11:08:35 +0200

    Update api.py

--- application/api.py
+++ application/api.py
@@ -13,22 +13,5 @@ sns.set_style('darkgrid')
 
 app = Flask(__name__)
 
-@app.route("/load_age_population", methods=["GET"])
-
-def load_age_population():
-
-    z = ZipFile("/Users/sylvaincarlevato/p7scoringopenclassrooms/data/data_final.zip")
-
-    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-
-    data_age = round((data["DAYS_BIRTH"]/365), 2)
-
-    return json.dumps(list(data_age))
-
-
- 
-
-
-
 if __name__ == "__main__":
     app.run(host="localhost", port="4001", debug=True)
