8d8a295b1c9d239d7670fa1d20ff16f5cda43802
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-15 17:26:47 +0200

    Update dashboard.py

--- application/dashboard.py
+++ application/dashboard.py
@@ -20,11 +20,20 @@ URL_API = "http://localhost:4001/"
 def main() :
 
     @st.cache
-    def load_data_data():
-        data_json = requests.get("http://localhost:4001/load_data/data")
-        data = data_json.json()
-        return data
-  
+    def load_data():
+        z = ZipFile("data/data_final.zip")
+        data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+
+        z = ZipFile("data/X_enc.zip")
+        sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+        
+        description = pd.read_csv("data/features_description.csv",
+                                  usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
+
+        target = data.iloc[:, -1:]
+
+        return data, sample, target, description
+
     def load_data2():
         z = ZipFile("data/X_enc.zip")
         sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
