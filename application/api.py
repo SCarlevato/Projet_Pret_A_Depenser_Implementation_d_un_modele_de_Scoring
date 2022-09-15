92406e7024484c205a99df89a6f36d1d2ab4e186
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-15 17:44:32 +0200

    Update api.py

--- application/api.py
+++ application/api.py
@@ -13,30 +13,16 @@ sns.set_style('darkgrid')
 
 app = Flask(__name__)
 
-@app.route("/load_data/data", methods=["GET"])
-def load_data_data():
+def load_data():
     z = ZipFile("data/data_final.zip")
-    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-    return data.to_json(orient='values')
-
-@app.route("/load_data/sample", methods=["GET"])
-def load_data_sample():
+    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
     z = ZipFile("data/X_enc.zip")
-    sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-    return sample.to_json(orient='values')
-
-@app.route("/load_data/description", methods=["GET"])
-def load_data_description():
+    sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
     description = pd.read_csv("data/features_description.csv",
-                              usecols=['Row', 'Description'], index_col=0, encoding='unicode_escape')
-    return description.to_json(orient='values')
-
-@app.route("/load_data/target", methods=["GET"])
-def load_data_target():
-    z = ZipFile("data/data_final.zip")
-    data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding='utf-8')
+                                  usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
     target = data.iloc[:, -1:]
-    return json.dumps(list(target))
+
+    return data, sample, target, description
 
 @app.route("/load_infos_gen/credit", methods=["GET"])
 def load_infos_gen_credit():
