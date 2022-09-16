92638008a8216b1117057d506dce7a26d680c143
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 09:50:40 +0200

    Update dashboard.py

--- application/dashboard.py
+++ application/dashboard.py
@@ -20,79 +20,7 @@ URL_API = "http://localhost:4001/"
 
 def main() :
 
-    @st.cache()
-    def load_data_data():
-        data_json = requests.get(URL_API + "load_data/data")
-        data = data_json.json()
-        return data
 
-    def load_model():
-        '''Chargement du Modèle Entraîné'''
-        pickle_in = open('modele/classifier_xgb_model.pkl', 'rb')
-        clf = pickle.load(pickle_in)
-        return clf
-
-    def identite_client(data, id):
-        data_client = data[data.index == int(id)]
-        return data_client
-
-     # Chargement des Données :
-    
-    data = load_data_data()
-    id_client = sample.index.values
-    clf = load_model()   
-    
-    @st.cache
-    def load_infos_gen(data):
-        lst_infos = [data.shape[0],
-                     round(data["AMT_INCOME_TOTAL"].mean(), 2),
-                     round(data["AMT_CREDIT"].mean(), 2)]
-
-        nb_credits = lst_infos[0]
-        rev_moy = lst_infos[1]
-        credits_moy = lst_infos[2]
-
-        targets = data.TARGET.value_counts()
-
-        return nb_credits, rev_moy, credits_moy, targets
-
-    @st.cache
-    def load_age_population(data):
-        data_age = round((data["DAYS_BIRTH"]/365), 2)
-        return data_age
-
-    @st.cache
-    def load_income_population(sample):
-        df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
-        df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
-        return df_income
-
-    @st.cache
-    def load_prediction(sample, id, clf):
-        X=sample.iloc[:, :-1]
-        score = clf.predict_proba(X[X.index == int(id)])[:,1]
-        return score
-
-    @st.cache
-    def knn_training(sample):
-        knn = KMeans(n_clusters=2).fit(sample)
-        return knn
-      
-    @st.cache(allow_output_mutation=True)
-    def load_knn(sample):
-        knn = knn_training(sample)
-        return knn
-
-    @st.cache
-    def load_kmeans(sample, id, mdl):
-        index = sample[sample.index == int(id)].index.values
-        index = index[0]
-        data_client = pd.DataFrame(sample.loc[sample.index, :])
-        df_neighbors = pd.DataFrame(knn.fit_predict(data_client), index=data_client.index)
-        df_neighbors = pd.concat([df_neighbors, data], axis=1)
-        return df_neighbors.iloc[:,1:].sample(10)
-
-    #######################################
     # SIDEBAR #
     #######################################
 
