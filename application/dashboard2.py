03ed0d1d55274c2684280057edff330e0db3e31c
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 09:44:28 +0200

    Update dashboard2.py

--- application/dashboard2.py
+++ application/dashboard2.py
@@ -20,32 +20,6 @@ URL_API = "http://localhost:4001/"
 def main() :
 
     @st.cache
-    def load_data():
-        z = ZipFile("data/data_final.zip")
-        data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
-
-        z = ZipFile("data/X_enc.zip")
-        sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
-        
-        description = pd.read_csv("data/features_description.csv",
-                                  usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
-
-        target = data.iloc[:, -1:]
-
-        return data, sample, target, description
-
-    def load_model():
-        '''Chargement du Modèle Entraîné'''
-        pickle_in = open('modele/classifier_xgb_model.pkl', 'rb')
-        clf = pickle.load(pickle_in)
-        return clf
-
-    @st.cache(allow_output_mutation=True)
-    def load_knn(sample):
-        knn = knn_training(sample)
-        return knn
-
-    @st.cache
     def load_infos_gen(data):
         lst_infos = [data.shape[0],
                      round(data["AMT_INCOME_TOTAL"].mean(), 2),
