becb744b71a67a7627f639ac06890533357a8bf2
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 09:52:23 +0200

    Update dashboard2.py

--- application/dashboard2.py
+++ application/dashboard2.py
@@ -8,16 +8,36 @@ import shap
 import plotly.express as px
 from zipfile import ZipFile
 from sklearn.cluster import KMeans
-import requests
-import json
-from PIL import Image
-
 plt.style.use('fivethirtyeight')
 sns.set_style('darkgrid')
 
-URL_API = "http://localhost:4001/"
-
 def main() :
+
+    @st.cache
+    def load_data():
+        z = ZipFile("data/data_final.zip")
+        data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+
+        z = ZipFile("data/X_enc.zip")
+        sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
+        
+        description = pd.read_csv("data/features_description.csv",
+                                  usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
+
+        target = data.iloc[:, -1:]
+
+        return data, sample, target, description
+
+    def load_model():
+        '''Chargement du Modèle Entraîné'''
+        pickle_in = open('modele/classifier_xgb_model.pkl', 'rb')
+        clf = pickle.load(pickle_in)
+        return clf
+
+    @st.cache(allow_output_mutation=True)
+    def load_knn(sample):
+        knn = knn_training(sample)
+        return knn
 
     @st.cache
     def load_infos_gen(data):
