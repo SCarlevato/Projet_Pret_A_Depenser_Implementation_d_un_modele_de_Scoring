778ef0b15711b68bcc19093b013c2a94bd101bcc
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 09:28:08 +0200

    Update and rename apinumber2.py to load_data_id_model_knn.py

--- application/modules_api/load_data_id_model_knn.py
+++ application/modules_api/load_data_id_model_knn.py
@@ -34,34 +34,9 @@ def load_knn(sample):
      knn = knn_training(sample)
      return knn
 
-def load_infos_gen(data):
-    lst_infos = [data.shape[0],
-                     round(data["AMT_INCOME_TOTAL"].mean(), 2),
-                     round(data["AMT_CREDIT"].mean(), 2)]
-
-     nb_credits = lst_infos[0]
-     rev_moy = lst_infos[1]
-     credits_moy = lst_infos[2]
-     targets = data.TARGET.value_counts()
-     return nb_credits, rev_moy, credits_moy, targets
-
 def identite_client(data, id):
      data_client = data[data.index == int(id)]
      return data_client
-
-def load_age_population(data):
-     data_age = round((data["DAYS_BIRTH"]/365), 2)
-     return data_age
-
-def load_income_population(sample):
-     df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
-     df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
-     return df_income
-
-def load_prediction(sample, id, clf):
-     X=sample.iloc[:, :-1]
-     score = clf.predict_proba(X[X.index == int(id)])[:,1]
-     return score
 
  def load_kmeans(sample, id, mdl):
      index = sample[sample.index == int(id)].index.values
@@ -74,8 +49,3 @@ def load_prediction(sample, id, clf):
  def knn_training(sample):
      knn = KMeans(n_clusters=2).fit(sample)
      return knn
-
- # Chargement des Données :
-    data, sample, target, description = load_data()
-    id_client = sample.index.values
-    clf = load_model()
