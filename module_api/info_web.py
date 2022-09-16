99403ce04439bd3e114c3781011eba24a626c265
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 10:50:14 +0200

    Update info_web.py

--- module_api/info_web.py
+++ module_api/info_web.py
@@ -26,6 +26,24 @@ def load_infos_gen(data):
     
     return jsonify({"nb_credits":nb_credits, "rev_moy": rev_moy, "credits_moy": credits_moy})
 
+@app.route("/load_age_population", methods=['POST'])
+
+def load_age_population():
+
+    data_age = round((data["DAYS_BIRTH"]/365), 2)
+    
+    return json.dumps(list(data_age))
+
+@app.route("/load_income_population", methods=['POST'])
+
+def load_income_population():
+
+    df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
+    
+    df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
+    
+    return df_income.to_json(orient='values')
+
 if __name__ == '__main__':
     try:
         port = int(system.argv[1]) # This is for a command-line input
