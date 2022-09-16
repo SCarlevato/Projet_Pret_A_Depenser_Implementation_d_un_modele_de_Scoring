fbed04da15f505a879c5942da3ceb16b3b703d48
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-16 10:34:31 +0200

    Update info_web.py

--- module_api/info_web.py
+++ module_api/info_web.py
@@ -10,16 +10,28 @@ from prediction_api import *
 # Your API definition
 app = Flask(__name__)
 
+@app.route('/infos_gen', methods=['POST'])
 
-
-@app.route('/predictByClientId', methods=['POST'])
-
+def load_infos_gen():
+    
+    lst_infos = [data.shape[0],round(data["AMT_INCOME_TOTAL"].mean(), 2),round(data["AMT_CREDIT"].mean(), 2)]
+    
+    targets = data.TARGET.value_counts()
+    
+    nb_credits = lst_infos[0]
+    
+    rev_moy = lst_infos[1]
+    
+    credits_moy = lst_infos[2]
+    
+    prop_default = targets
+    
+    return jsonify({"nb_credits":nb_credits, "rev_moy": rev_moy, "credits_moy": credits_moy})
 
 if __name__ == '__main__':
     try:
         port = int(system.argv[1]) # This is for a command-line input
     except:
         port = 12345 # If you don't provide any port the port will be set to 12345
-
 
     app.run(port=port, debug=True)
