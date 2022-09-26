a13df7fb8ee15c0b3e85bc74adc3198a3fbfc9cc
SCarlevato <74147682+SCarlevato@users.noreply.github.com>
2022-09-26 18:28:53 +0200

    Update load_data_id_model_knn.py

--- module_api/load_data_id_model_knn.py
+++ module_api/load_data_id_model_knn.py
@@ -19,15 +19,15 @@ sns.set_style('darkgrid')
 
 def load_data():
     
-    z = ZipFile("data/data_final.zip")
+    z = ZipFile("DATASETS/data_final.zip")
     
     data = pd.read_csv(z.open('data_final.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
                        
-    z = ZipFile("data/X_enc.zip")
+    z = ZipFile("DATASETS/X_enc.zip")
                        
     sample = pd.read_csv(z.open('X_enc.csv'), index_col='SK_ID_CURR', encoding ='utf-8')
                        
-    description = pd.read_csv("data/features_description.csv", usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
+    description = pd.read_csv("DATASETS/features_description.csv", usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
                        
     target = data.iloc[:, -1:]
                        
@@ -37,7 +37,7 @@ def load_model():
                        
     '''Chargement du Modèle Entraîné'''
                        
-    pickle_in = open('modele/classifier_xgb_model.pkl', 'rb')
+    pickle_in = open('MODELE/classifier_xgb_model.pkl', 'rb')
                        
     clf = pickle.load(pickle_in)
                        
